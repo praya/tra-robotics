8e5905f104751d480baad9c6e5b2aa431428b4e8
Andrey Bezrodnov <a.bezrodnov@corp.mail.ru>
2019-10-20 21:29:35 +0300

    compare component for storybook

---
+++ src/components/utils/Compare.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+
+const ToggleButton = styled.button`
+    outline: none;
+    border: 1px solid #dddddd;
+    border-radius: 3px;
+    background: #eeeeee;
+    margin-bottom: 10px;
+    
+    &:active {
+        background: #f5f5f5;
+    }
+`;
+
+const mockImg = (mockup: string) => <img alt="mockup" srcSet={`${mockup} 2x`} />;
+
+
+interface CompareProps {
+    mockup: string;
+    children: React.ReactNode;
+}
+
+export const Compare: React.FC<CompareProps> = ({ mockup, children }) => {
+    const [showMockup, setShowMockup] = React.useState(false);
+
+    return (
+        <div>
+            <ToggleButton
+                onMouseDown={() => setShowMockup(true)}
+                onMouseUp={() => setShowMockup(false)}
+            >
+                Show mockup
+            </ToggleButton>
+            <div style={{ display: showMockup ? 'block' : 'none' }}>
+                {mockImg(mockup)}
+            </div>
+            <div style={{ display: showMockup ? 'none' : 'block' }}>
+                {children}
+            </div>
+        </div>
+    )
+}
