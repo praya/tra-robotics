8f5f38d14404607d17499f103f3e9dbe5e7f3f28
Andrey Bezrodnov <a.bezrodnov@corp.mail.ru>
2019-10-23 04:26:53 +0300

    headerr component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,11 @@ import styled from 'styled-components';
 
 
 export const Header = styled.div`
+    font-size: 32px;
+    line-height: 24px;
     background: #f4f5f6;
+    color: rgba(63, 68, 72, 0.6);
     box-shadow: inset 0px -1px 0px #d6dadb;
-    padding: 15px 24px;
+    padding: 15px 24px 17px 24px;
+    font-family: Roobert, sans-serif;
 `;
