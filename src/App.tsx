2e5366c4be575c57cd19190e634a3c6c3f846ab9
Andrey Bezrodnov <a.bezrodnov@corp.mail.ru>
2019-10-23 04:57:16 +0300

    update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AssemblyProcessesView } from './components/assembly-processes-view/AssemblyProcessesView';
+import { State } from './components/toggle/Toggle';
+import { SubMenu } from './components/menu/Menu';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const priorities: [State, State] = [
+    { id: 'latest_first', title: 'Latest first' },
+    { id: 'oldest_first', title: 'Old first' },
+];
+
+export const filters: SubMenu[] = [
+    {
+        id: 'assembly',
+        title: 'Assembly',
+        items: [
+            { id: 'any', title: 'Any', selected: true },
+            { id: 'in_review', title: 'In Review' },
+            { id: 'review_finished', title: 'Review Finished' },
+        ]
+    },
+    {
+        id: 'review',
+        title: 'Review',
+        items: [
+            { id: 'any', title: 'Any', selected: true },
+            { id: 'draft', title: 'Draft' },
+            { id: 'solved', title: 'Solved' },
+            { id: 'simulation_requested', title: 'Simulation Requested' },
+            { id: 'simulation_positive', title: 'Simulation Positive' },
+            { id: 'simulation_negative', title: 'Simulation Negative' },
+        ]
+    }
+];
+
+export const App: React.FC = () => {
+    return (
+        <div>
+            <AssemblyProcessesView
+                items={[]}
+                filters={filters}
+                priorities={priorities}
+                priority={priorities[0].id}
+                onPriorityChange={console.info}
+            />
+        </div>
+    );
+};
